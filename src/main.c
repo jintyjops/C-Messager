440aa34160ebe68a125679c9954e0d77349fa8e7
jintyjops <patricksheppardd@gmail.com>
2017-02-08 23:27:30 +0000

    Done with testing...

--- src/main.c
+++ src/main.c
@@ -6,9 +6,5 @@
 int main(int argc, char** argv){
     // DO THINGS!
 
-    // delete following comments later...
-    // This is a test comment... delete later.
-    // This should be creates on testing 2
-
     return EXIT_SUCCESS;
 }
